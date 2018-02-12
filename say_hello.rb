ce81984f7ea7a9a33f4d632f2670ab367b54c8b8
Ervin Melara <ervin.melara@wework.com>
2018-02-12 21:38:38 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,4 @@
-# Build your say_hello method here
+say_hello = "Ervin"
+
+def say_hello(name = Ruby Programmer)
+end
--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -12,3 +12,8 @@ describe "say_hello" do
     say_hello()
   end
 end
+
+def say_hello(name = "Hello Ruby Programmer!")
+end
+
+say_hello("Ervin")
